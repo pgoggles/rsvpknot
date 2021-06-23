5bc01a89a7a79ebf5ac84b108f94c7eadd240af4
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-23 09:52:03 -0700

    Add Babel Transpiling

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,18 @@ module.exports = {
     path: path.resolve(__dirname, 'client', 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
