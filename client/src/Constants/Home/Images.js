709cc03e3e913b2ae7d44a66fe46b20bb08d3d81
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-23 11:05:02 -0700

    Add Constants Folder with Carousel Images

---
+++ client/src/Constants/Home/Images.js
@@ -0,0 +1,11 @@
+const Carousel = [
+  'https://images.unsplash.com/photo-1465495976277-4387d4b0b4c6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+  'https://images.unsplash.com/photo-1529636798458-92182e662485?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+  'https://images.unsplash.com/photo-1522673607200-164d1b6ce486?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+];
+
+const ImageConstants = {
+  Carousel
+};
+
+export default ImageConstants;
