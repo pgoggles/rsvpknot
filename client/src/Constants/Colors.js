6e96ae7f19219e590698e80b7cc2c7290e0f3943
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-23 18:07:00 -0700

    Add Constant Colors

---
+++ client/src/Constants/Colors.js
@@ -0,0 +1,10 @@
+const colorPalette = {
+  pink: '#FFB7A1',
+  lightpink: '#ffccbd',
+  lightpurple: '#c47eb6',
+  gold: '#EFBC68',
+  sky: '#C2D7D0',
+  teal: '#5F9595'
+};
+
+export default colorPalette;
