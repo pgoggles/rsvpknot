d2dd6f705dc115811e59992bd02d3a418b1faecd
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-23 18:12:45 -0700

    Add Hover Styling to NavBar

--- client/src/Styles/NavBar.js
+++ client/src/Styles/NavBar.js
@@ -4,18 +4,23 @@ import ColorPalette from '../Constants/Colors';
 const NavBar = styled.div `
   width: 100%;
   height: 80px;
-  background-color: ${ColorPalette.lightpink};
+  background-color: ${ColorPalette.pink};
   display: flex;
   align-items: center;
   justify-content: space-evenly;
   color: white;
-`;
+  `;
 
 const NavButton = styled.div `
   padding: 0.3%;
   border-radius: 5px;
   border-color: white;
   border-style: solid;
+  &:hover {
+    border-color: ${ColorPalette.lightred};
+    color: ${ColorPalette.lightred};
+    cursor: pointer;
+  }
 `;
 
 const styles = {
