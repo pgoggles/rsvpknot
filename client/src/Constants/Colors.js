bf4554c9ef37a217a29e18874f2f7293ce30d558
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-06-23 18:12:34 -0700

    Add Light Red

--- client/src/Constants/Colors.js
+++ client/src/Constants/Colors.js
@@ -2,9 +2,10 @@ const colorPalette = {
   pink: '#FFB7A1',
   lightpink: '#ffccbd',
   lightpurple: '#c47eb6',
+  lightred: '#e69393',
   gold: '#EFBC68',
   sky: '#C2D7D0',
-  teal: '#5F9595'
+  teal: '#5F9595',
 };
 
 export default colorPalette;
